1981406d2bd2d645cfa2d84f585806341976abe6
vladimir <vovka1998@gmail.com>
2024-12-05 15:14:27 +0300

    Переписал ORM на асинхрон

--- middlewares/user.py
+++ middlewares/user.py
@@ -15,7 +15,7 @@ class MyMiddleware(BaseMiddleware):
             data: Dict[str, Any]) -> Any:
         tg_id_user = event.from_user.id
         user_db = UserORM()
-        user: UserTable = user_db.get_user(tg_id_user)
+        user: UserTable = await user_db.get_user(tg_id_user)
         if user:
             data["user_table"] = user
             return await handler(event, data)
@@ -23,6 +23,6 @@ class MyMiddleware(BaseMiddleware):
             username = event.from_user.username
             name = event.from_user.first_name
             user_db.add_user(username=username, name=name, id_telegram=tg_id_user)
-            user: UserTable = user_db.get_user(tg_id_user)
+            user: UserTable = await user_db.get_user(tg_id_user)
             data["user_table"] = user
             return await handler(event, data)
