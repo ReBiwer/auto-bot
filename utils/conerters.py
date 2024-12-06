e3a324a3d1e35016cc42f0a9138d83c9ef3024d3
vladimir <vovka1998@gmail.com>
2024-12-06 14:03:24 +0300

    Обновил регулярное выражение

--- utils/conerters.py
+++ utils/conerters.py
@@ -3,7 +3,10 @@ from decimal import Decimal
 
 
 def convert_to_decimal(value: str):
-    decimal_pattern = r'\d{1,},\d{2}'
-    re_value = re.search(decimal_pattern, value).group(0)
+    decimal_pattern = r'\d{1,}[\,\.]\d{2}'
+    re_value = re.search(
+        decimal_pattern,
+        value.replace(' ', '')
+    ).group(0)
     new_value = re_value.replace(',', '.')
     return Decimal(new_value)
