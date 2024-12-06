dee6de1fbf7712b241292d6589af969b640eb5a4
vladimir <vovka1998@gmail.com>
2024-12-06 17:38:53 +0300

    Добавил клавиатуры для работы с деталями заправок

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -1,3 +1,5 @@
+from db_handler.models import RefuelingTable
+
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
@@ -8,3 +10,19 @@ def get_inline_kb_check_data() -> InlineKeyboardMarkup:
         [InlineKeyboardButton(text='Не верно', callback_data='incorrect')],
     ]
     return InlineKeyboardMarkup(inline_keyboard=inline_but)
+
+
+def get_inline_kb_refuels(refuels: dict[int, RefuelingTable]) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+    for id_refuel, refuel in refuels.items():
+        builder.row(
+            InlineKeyboardButton(text=f'{refuel.date}',
+                                 callback_data=f'detail_refuel_{id_refuel}')
+        )
+    builder.adjust(1)
+    return builder.as_markup()
+
+
+def get_inline_butt_back():
+    but = InlineKeyboardButton(text='Обратно к списку заправок', callback_data='go_to_refuels')
+    return InlineKeyboardMarkup(inline_keyboard=[[but]])
