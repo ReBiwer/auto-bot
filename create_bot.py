9190be5e5ff7d1b2a39d5e5dc8676dfa226e1d27
vladimir <vovka1998@gmail.com>
2025-01-12 19:33:35 +0300

    Подключил редис к боту

--- create_bot.py
+++ create_bot.py
@@ -5,11 +5,18 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 dotenv.load_dotenv()
-# pg_db = PostgresHandler(config('PG_LINK'))
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_NUM_DB = os.environ.get('REDIS_NUM_DB')
+
+redis_url = f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_NUM_DB}'
+
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 admins = [int(admin_id) for admin_id in os.environ.get('ADMINS').split(',')]
 
@@ -17,4 +24,5 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 logger = logging.getLogger(__name__)
 
 bot = Bot(token=os.environ.get('BOT_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
+storage = RedisStorage.from_url(redis_url)
+dp = Dispatcher(storage=storage)
