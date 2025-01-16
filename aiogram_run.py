bdf2b48c0cbcf27e3ad5295b1e9624181f430586
vladimir <vovka1998@gmail.com>
2025-01-16 17:38:08 +0300

    Добавил команду change_refueling и подключил роутер

--- aiogram_run.py
+++ aiogram_run.py
@@ -3,14 +3,18 @@ from create_bot import bot, dp, scheduler
 from handlers.start import start_router
 from handlers.add_refueling import add_refuel_router
 from handlers.detail_refueling import detail_refueling_router
+from handlers.change_refueling import change_refueling_router
 from aiogram.types import BotCommand, BotCommandScopeDefault
 # from work_time.time_func import send_time_msg
 
 
 async def set_commands():
-    commands = [BotCommand(command='start', description='Старт'),
-                BotCommand(command='add_refueling', description='Добавить заправку'),
-                BotCommand(command='detail_refueling', description='Узнать о своих заправках'),]
+    commands = [
+        BotCommand(command='start', description='Старт'),
+        BotCommand(command='add_refueling', description='Добавить заправку'),
+        BotCommand(command='change_refueling', description='Изменить заправку'),
+        BotCommand(command='detail_refueling', description='Узнать о своих заправках'),
+    ]
     await bot.set_my_commands(commands, BotCommandScopeDefault())
 
 
@@ -21,10 +25,12 @@ async def main():
         start_router,
         add_refuel_router,
         detail_refueling_router,
+        change_refueling_router,
     )
     await bot.delete_webhook(drop_pending_updates=True)
     await set_commands()
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
