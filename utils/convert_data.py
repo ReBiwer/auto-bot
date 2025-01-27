c28496656c4ed6dc3524276aa99be4f90e8fc390
vladimir <vovka1998@gmail.com>
2025-01-27 19:17:38 +0300

    Добавил функцию для извлечения данных о заправках и FSMContext

--- utils/convert_data.py
+++ utils/convert_data.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.context import FSMContext
 
-from db_settings.DTO_models import UserGetDTO
+from db_settings.DTO_models import UserGetDTO, RefuelGetDTO
 
 
 async def get_user_info(state: FSMContext) -> UserGetDTO:
@@ -12,3 +12,21 @@ async def get_user_info(state: FSMContext) -> UserGetDTO:
     data_state = await state.get_data()
     user_data = data_state['user']
     return UserGetDTO.model_validate_json(user_data)
+
+
+async def get_refuels_info(state: FSMContext) -> dict[int, RefuelGetDTO] | None:
+    """
+    Функция для извлечения информации о заправках сохраненной в state
+    :param state:
+    :return:
+    """
+    data_state = await state.get_data()
+    if data_state['refuels'] is not None:
+        refuels_data: dict[int, str] = data_state['refuels']
+        refuels = {
+            key: RefuelGetDTO.model_validate(refuel)
+            for key, refuel in refuels_data.items()
+        }
+        return refuels
+    else:
+        return None
